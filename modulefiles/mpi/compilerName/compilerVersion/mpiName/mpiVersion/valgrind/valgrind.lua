e0bcc0862dc19c5eb4319d997be3a0ed926ce64a
Rahul Mahajan <aerorahul@users.noreply.github.com>
2019-05-03 14:20:18 -0400

    WIP: add script to build valgrind on OSX. Does not work on macOS Mojave
    Change-Id: Ic743c9b2eb07f955eca8211e8b1149c87c28f7e2

---
+++ modulefiles/mpi/compilerName/compilerVersion/mpiName/mpiVersion/valgrind/valgrind.lua
@@ -0,0 +1,34 @@
+help([[
+]])
+
+local pkgName    = myModuleName()
+local pkgVersion = myModuleVersion()
+local pkgNameVer = myModuleFullName()
+
+local hierA        = hierarchyA(pkgNameVer,2)
+local mpiNameVer   = hierA[1]
+local compNameVer  = hierA[2]
+local mpiNameVerD  = mpiNameVer:gsub("/","-")
+local compNameVerD = compNameVer:gsub("/","-")
+
+conflict(pkgName)
+
+local opt = os.getenv("OPT") or "/opt"
+
+local base = pathJoin(opt,compNameVerD,mpiNameVerD,pkgName,pkgVersion)
+
+prepend_path("PATH", pathJoin(base,"bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base,"lib"))
+prepend_path("DYLD_LIBRARY_PATH", pathJoin(base,"lib"))
+prepend_path("CPATH", pathJoin(base,"include"))
+prepend_path("MANPATH", pathJoin(base,"share","man"))
+
+setenv("VALGRIND_ROOT", base)
+setenv("VALGRIND_INCLUDES", pathJoin(base,"include"))
+setenv("VALGRIND_LIBRARIES", pathJoin(base,"lib"))
+setenv("VALGRIND_VERSION", pkgVersion)
+
+whatis("Name: ".. pkgName)
+whatis("Version: " .. pkgVersion)
+whatis("Category: Tools")
+whatis("Description: Debugging and Profiling tool")
